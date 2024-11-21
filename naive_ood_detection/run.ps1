ab1b72e5275baa0cba6ad62ffb33d75ef9c694b6
Edoardo Cappelli <98023490+EdoardoCappelli@users.noreply.github.com>
2024-11-21 22:06:17 +0100

    Create run.ps1

---
+++ naive_ood_detection/run.ps1
@@ -0,0 +1,5 @@
+python train.py --batch_size 32 --epochs 10 --learning_rate 0.001 --checkpoint_name "cifar10_CNN_10.pth"
+
+python train.py --batch_size 32 --epochs 50 --learning_rate 0.001 --checkpoint_name "cifar10_CNN_50.pth"
+
+python train.py --batch_size 32 --epochs 100 --learning_rate 0.001 --checkpoint_name "cifar10_CNN_100.pth"
